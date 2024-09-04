e09b1ffa626e8922739320b930a81d76c1c7c824
bofafa <bopuinar@gmail.com>
2024-09-04 11:33:04 +0800

    update

--- Test-out/src/IfKeywordAndCodeBlocks/Main.java
+++ Test-out/src/IfKeywordAndCodeBlocks/Main.java
@@ -1,18 +1,24 @@
+package IfKeywordAndCodeBlocks;
+
 public class Main {
 
     public static void main(String[] args) {
-
         boolean gameOver = true;
         int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int levelCompleted =5;
+        int bonus =100;
+    }
+    
+        public static void calculateScore( boolean gameOver ,  int score,int levelCompleted ,int bonus ){
+ 
 
-        if (score < 5000 && score > 1000) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000) {
-            System.out.println("Your score was less than 1000");
-        } else {
-            System.out.println("Got here");
-        }
+            int finalScore = score;
+        
+            if (gameOver){
+                finalScore += (levelCompleted * bonus);
+                 finalScore +=1000;
+                System.out.println("your final score was " +finalScore); //1300
+            }
+
     }
 }
--- Test-out/src/IfKeywordAndCodeBlocks/MainChallenge.java
+++ Test-out/src/IfKeywordAndCodeBlocks/MainChallenge.java
@@ -1,37 +1,31 @@
+package IfKeywordAndCodeBlocks;
+
 public class MainChallenge {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+      
+    
+       
+    calculateScore(true, 800, 5, 100);
+    calculateScore(true, 1000, 8, 200);
 
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("The highScore is " + highScore);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
 
-        System.out.println("The next highScore is " +
-                calculateScore(gameOver, score, levelCompleted, bonus));
+
     }
+// make Method
+//void nothing return
+    public static void calculateScore( boolean gameOver ,  int score,int levelCompleted ,int bonus ){
+ 
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    int finalScore = score;
 
-        int finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-        }
-
-        return finalScore;
-
-
-
-     
+    if (gameOver){
+        finalScore += (levelCompleted * bonus);
+         finalScore +=1000;
+        System.out.println("your final score was " +finalScore); //1300
+    }
     }
 }
 
